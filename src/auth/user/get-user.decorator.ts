b5f8d11bd66fca294ab7b8e3bdd42258d562c58a
dimitrios.kiriakakis <dimitrios.kiriakakis@lotto24.de>
2021-07-07 13:15:33 +0000

    Add return type to user decorator.

--- src/auth/user/get-user.decorator.ts
+++ src/auth/user/get-user.decorator.ts
@@ -1,7 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from './user.entity';
 
-export const GetUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  return request.user as User;
-});
+export const GetUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): User => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user as User;
+  },
+);
